2092030e5ce3e8f0f4797a608631364b2a9f3461
“Qonita <qonita.adestyanti@ui.ac.id>
2026-03-12 14:49:05 +0700

    Do pmd analysis v2

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EshopApplication {
+	protected EshopApplication() {
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EshopApplication.class, args);
 	}
-
 }
