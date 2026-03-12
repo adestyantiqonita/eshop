7e1cb9a70770f2af793de6d25f01a39dd9deb617
“Qonita <qonita.adestyanti@ui.ac.id>
2026-03-12 14:56:48 +0700

    Do pmd analysis v2.1

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -5,7 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EshopApplication {
+
+	/**
+	 * Protected constructor to prevent instantiation of this utility class.
+	 * PMD requires a comment inside empty constructors.
+	 */
 	protected EshopApplication() {
+		// This constructor is intentionally empty.
 	}
 
 	public static void main(String[] args) {
