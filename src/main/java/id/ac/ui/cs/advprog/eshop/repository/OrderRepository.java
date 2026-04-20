e08db55789718428f1fbdcbc9fb223015abe88b0
“Qonita <qonita.adestyanti@ui.ac.id>
2026-04-20 19:08:46 +0700

    [GREEN] Implement OrderRepository class.

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -10,14 +10,39 @@ public class OrderRepository {
     private List<Order> orderData = new ArrayList<>();
 
     public Order save(Order order) {
-        return null;
+        int index = -1;
+        for (int i = 0; i < orderData.size(); i++) {
+            if (orderData.get(i).getId().equals(order.getId())) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1) {
+            orderData.add(order);
+            return order;
+        } else {
+            orderData.set(index, order);
+            return order;
+        }
     }
 
     public Order findById(String id) {
+        for (Order order : orderData) {
+            if (order.getId().equals(id)) {
+                return order;
+            }
+        }
         return null;
     }
 
     public List<Order> findAllByAuthor(String author) {
-        return null;
+        List<Order> result = new ArrayList<>();
+        for (Order order : orderData) {
+            if (order.getAuthor().equals(author)) {
+                result.add(order);
+            }
+        }
+        return result;
     }
 }
