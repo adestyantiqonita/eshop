0bc0a1a3597de35fed9da0625bee169d941167ee
“Qonita <qonita.adestyanti@ui.ac.id>
2026-04-21 13:20:34 +0700

    [GREEN] Implement Payment model class

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+import lombok.Getter;
+import java.util.Map;
+
+@Getter
+public class Payment {
+    private String id;
+    private String method;
+    private String status;
+    private Order order;
+    private Map<String, String> paymentData;
+
+    public Payment(String id, Order order, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.order = order;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.status = PaymentStatus.WAITING_PAYMENT.getValue();
+    }
+}
